55509e2351ef3769d5a6cf319db4ce039aacb9c9
Faid Terence JABO <faidterence@outlook.com>
2023-09-06 19:49:46 +0200

    Arithmetic Operations

---
+++ Microsoft/Arithmetics.cs
@@ -0,0 +1,17 @@
+int a =9, b= 3;
+
+int sum = a+b;
+int diff = a-b;
+int mult = a * b;
+int div = a /b ;
+int result = 3 + 1 * 5 / 2;
+int first = 8;
+int second =3;
+decimal quotient = (decimal)first / (decimal)second;
+Console.WriteLine("Sum is: " + sum);
+Console.WriteLine("Difference: " +diff);
+Console.WriteLine("Multiplication: " + mult);
+Console.WriteLine("Quotient is: " +div);
+Console.WriteLine(result);
+Console.WriteLine(quotient);
+
